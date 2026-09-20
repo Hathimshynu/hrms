a02f334cd9f8223311c432e56859e69b8448ad78
Shynu <shynushyni55@gmail.com>
2026-09-20 16:57:55 +0530

    new changes

---
+++ hrms-backend/docker-start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sed -i "s/Listen 80/Listen ${PORT:-10000}/" /etc/apache2/ports.conf
+sed -i "s/:80/:${PORT:-10000}/" /etc/apache2/sites-available/000-default.conf
+php artisan config:cache
+php artisan route:cache
+php artisan migrate --force
+exec apache2-foreground
