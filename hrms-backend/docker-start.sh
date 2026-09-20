d50565bdf6dce46d6919009561024f78dbaa3eef
Shynu <shynushyni55@gmail.com>
2026-09-20 17:09:14 +0530

    new changes

--- hrms-backend/docker-start.sh
+++ hrms-backend/docker-start.sh
@@ -4,4 +4,9 @@ sed -i "s/:80/:${PORT:-10000}/" /etc/apache2/sites-available/000-default.conf
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+
+if [ "$RUN_SEED" = "true" ]; then
+  php artisan db:seed --force
+fi
+
 exec apache2-foreground
